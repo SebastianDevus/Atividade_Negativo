bba1e25b2ab64c520bb49a87f9ff0b179b9131de
SebastianDevus <sebastianjogosextra@gmail.com>
2022-04-02 01:37:22 -0300

    Verificador de negatividade concluido

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int numeroDigitado;
+
+Console.WriteLine();
+
+Console.Write("Digite um número: ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+if (numeroDigitado < 0)
+{
+    Console.WriteLine("O número é negativo");
+}
+
+Console.WriteLine();
